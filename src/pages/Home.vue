464854dd5176bcedae6789617bd523d5b77acaf0
Kelvin Woo <kelvin.woo@gmail.com>
2022-11-17 22:58:03 -0800

    Adding iconoir

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -8,9 +8,16 @@ useMeta({
 </script>
 
 <template>
-  <h1>Home</h1>  
+  <div
+    class="text-center flex items-center justify-center"
+    style="height:100vh;"
+  >
+    <h1><i class="iconoir-peace-hand" /> Home</h1>
+  </div>
 </template>
 
 <style scoped lang="postcss">
-
+i{ 
+  font-weight:100;
+}
 </style>
