606c2f85336ce4b70ddd18a5ceff4d05a3d5f3b0
Kelvin Woo <kelvin.woo@gmail.com>
2023-10-05 22:36:57 +0000

    updating default appearances with support for themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: ["light"],
+  },
   plugins: [require("daisyui")],
-}
+};
